b39c6383b5105bb38c21a192f0c36086c83dd929
Stefanus Nanami <saint_nus@mac.com>
2022-09-14 22:19:15 +0900

    Initial Commit

---
+++ DebugLua/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DebugLua
+//
+//  Created by 七海ステファヌス on 2022/09/14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
