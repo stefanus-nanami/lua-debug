f3c12fc364388b96b6ea836a54f36a820f449457
Stefanus Nanami <saint_nus@mac.com>
2022-09-15 00:50:19 +0900

    First commit.

--- DebugLua/main.cpp
+++ DebugLua/main.cpp
@@ -1,14 +1,81 @@
-//
-//  main.cpp
-//  DebugLua
-//
-//  Created by 七海ステファヌス on 2022/09/14.
-//
-
+#include <cstdio>
 #include <iostream>
+#include <vector>
+#include <unistd.h>
 
-int main(int argc, const char * argv[]) {
-  // insert code here...
-  std::cout << "Hello, World!\n";
-  return 0;
+#include "lua.hpp"
+#include "LuaBridge/LuaBridge.h"
+#include "LRDB/server.hpp"
+
+using namespace std;
+using namespace luabridge;
+using namespace lrdb;
+
+class LuaApp
+{
+	lua_State* mLuaState;
+	vector< char > mPackagePath;
+  const char* mScriptPath;
+
+	void setupEnvironment() {
+	  LuaRef package = getGlobal(mLuaState, "package");
+
+		const char* lua_file = "?.lua";
+		
+		int length = snprintf(nullptr, 0, "%s/%s", mScriptPath, lua_file);
+	  mPackagePath.reserve(length + 1);
+
+		snprintf(mPackagePath.data(), mPackagePath.capacity(),	"%s/%s", mScriptPath, lua_file);
+		cout << "Package path = " << mPackagePath.data() << endl;
+
+		package["path"] = static_cast< const char* >(mPackagePath.data());
+	}
+
+public:
+	LuaApp(const char* script_path)
+		: mLuaState(nullptr),
+      mScriptPath(script_path)
+	{
+		lua_State* luaState = luaL_newstate();
+		if (luaState != nullptr) {
+			mLuaState = luaState;
+			luaL_openlibs(mLuaState);
+			setupEnvironment();
+		}
+	}
+
+	~LuaApp() {
+		if(mLuaState != nullptr) {
+			luaL_openlibs(mLuaState);
+			lua_close(mLuaState);
+			mLuaState = nullptr;
+		}
+	}
+
+	void execute() {
+	  lrdb::server debug_server(21110);
+	  debug_server.reset(mLuaState);
+
+		lua_getglobal(mLuaState, "require");
+		lua_pushstring(mLuaState, "main");
+		int err = lua_pcall(mLuaState, 1, 0, 0);
+		if(err != LUA_OK) {
+			cout << "Failed to run main." << endl;
+		}
+
+		debug_server.reset();
+	}
+
+};
+
+int main(int argc, const char** argv)
+{
+  if(argc != 2) {
+    cout << "Usage: DebugLua [script_path]" << endl;
+    return 1;
+  }
+
+  LuaApp app(argv[1]);
+	app.execute();
+	return 0;
 }
---
+++ README.md
@@ -0,0 +1,9 @@
+# debug-lua
+
+Debugging Lua with LRDB.
+
+Usage:
+1. Build DebugLua using Xcode.
+2. Run DebugLua
+3. Open project directory with Visual Studio Code.
+4. Run attach to host.
